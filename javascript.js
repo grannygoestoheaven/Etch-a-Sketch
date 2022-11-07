41a96e641cb0305f672007631c1ae23173702334
grannygoestoheaven <myganou@hotmail.com>
2022-11-07 11:11:32 +0100

    reset_grid_onclick

--- javascript.js
+++ javascript.js
@@ -10,24 +10,32 @@ const grid = document.createElement('div');
 grid.classList.add('grid');
 document.body.append(grid);
 
+
 button.addEventListener('click', () => {
-   let userEntry = prompt("Choose number of squares per side (max 100) :");
-   const SQUARECOUNT = (userEntry * userEntry);
-   
-   function createGridOfSquares()
-   {
-       // let's make the grid 
-       grid.style.gridTemplateColumns = `repeat(${userEntry}, 1fr)`;
-       grid.style.gridTemplateRows = `repeat(${userEntry}, 1fr)`;
-       
-       //let's fill the grid
-       for(i = 0; i < SQUARECOUNT; i++)
-       {
-           const squareDiv = document.createElement('div');
-           squareDiv.classList.add('square');
-           grid.append(squareDiv);
-       }
-   }
-   createGridOfSquares();
+    while (grid.lastChild) {
+        grid.removeChild(grid.lastChild);
+    };
+})
+
+button.addEventListener('click', () => {
+    let squarecount;
+    let userEntry = prompt("Choose number of squares per side (max 100) :");
+    
+    createGridOfSquares();
+    
+    function createGridOfSquares()
+    {
+        squarecount = (userEntry * userEntry);
+        grid.style.gridTemplateColumns = `repeat(${userEntry}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${userEntry}, 1fr)`;
+    
+        //let's fill the grid
+        for(i = 0; i < squarecount; i++)
+        {
+            const squareDiv = document.createElement('div');
+            squareDiv.classList.add('square');
+            grid.append(squareDiv);
+        }
+    }
 });
 
